636b5a323a8be4947371c0ba502714042ddc684a
Ola Jeppsson <ola.jeppsson@gmail.com>
2018-12-03 15:58:48 -0500

    hwtracing: owl: Add OWL tracectrl driver
    Signed-off-by: Ola Jeppsson <ola.jeppsson@gmail.com>

---
+++ drivers/hwtracing/owl/tracectrl.c
@@ -0,0 +1,118 @@
+/*
+ * OWL Trace device driver
+ *
+ * Copyright (C) 2018 Clemson University
+ * Written by Ola Jeppsson <ola.jeppsson@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * The full GNU General Public License is included in this distribution in the
+ * file called COPYING.
+ */
+
+#include <linux/bitops.h>
+#include <linux/gpio/driver.h>
+#include <linux/init.h>
+#include <linux/interrupt.h>
+#include <linux/io.h>
+#include <linux/module.h>
+#include <linux/platform_device.h>
+#include <linux/of.h>
+#include <linux/spinlock.h>
+
+#define DRV_NAME "riscv-tracectrl"
+
+#define TRACECTRL_CONFIG	0x00 /* config register */
+#define TRACECTRL_STATUS	0x04 /* status register */
+#define TRACECTRL_BUF0_ADDR	0x10 /* base address of trace buffer 0 */
+#define TRACECTRL_BUF0_MASK	0x18 /* mask size of trace buffer 0 */
+
+
+struct tracectrl {
+	void __iomem *base_addr;
+	spinlock_t lock;
+};
+
+static inline void tracectrl_reg_write(u32 value, struct tracectrl *ctrl,
+				     unsigned long offset)
+{
+	writel(value, (u8 __iomem *) ctrl->base_addr + offset);
+}
+
+static inline u32 tracectrl_reg_read(struct tracectrl *ctrl, unsigned long offset)
+{
+	return readl((u8 __iomem *) ctrl->base_addr + offset);
+}
+
+static const struct of_device_id tracectrl_of_match[] = {
+	{ .compatible = "clemson,trace-ctrl" },
+	{ }
+};
+MODULE_DEVICE_TABLE(of, tracectrl_of_match);
+
+/**
+ * tracectrl_probe - Platform probe for a tracectrl device
+ * @pdev:	platform device
+ *
+ * Note: All interrupts are cleared + masked after function exits.
+ *
+ * Return: 0 on success, negative error otherwise.
+ */
+static int tracectrl_probe(struct platform_device *pdev)
+{
+	struct tracectrl *ctrl;
+	struct resource *res;
+
+	ctrl = devm_kzalloc(&pdev->dev, sizeof(*ctrl), GFP_KERNEL);
+	if (!ctrl)
+		return -ENOMEM;
+
+	platform_set_drvdata(pdev, ctrl);
+
+	res = platform_get_resource(pdev, IORESOURCE_MEM, 0);
+	ctrl->base_addr = devm_ioremap_resource(&pdev->dev, res);
+	if (IS_ERR(ctrl->base_addr))
+		return PTR_ERR(ctrl->base_addr);
+
+	spin_lock_init(&ctrl->lock);
+
+	/* TODO: Let kernel allocate buffer ... */
+	/* TODO: 64 bit write ... */
+	tracectrl_reg_write(0x00701000, ctrl, TRACECTRL_BUF0_ADDR);
+	tracectrl_reg_write(0x00000001, ctrl, TRACECTRL_BUF0_ADDR + 4);
+	tracectrl_reg_write(0x00000fff, ctrl, TRACECTRL_BUF0_MASK);
+	tracectrl_reg_write(0x00000000, ctrl, TRACECTRL_BUF0_MASK + 4);
+
+	/* HACK: Enable trace ctrl. Should be user knob */
+	tracectrl_reg_write(1, ctrl, TRACECTRL_CONFIG);
+
+	return 0;
+}
+
+/**
+ * tracectrl_remove - Driver removal function
+ * @pdev:	platform device
+ *
+ * Return: 0 always
+ */
+static int tracectrl_remove(struct platform_device *pdev)
+{
+	return 0;
+}
+
+static struct platform_driver tracectrl_driver = {
+	.driver	= {
+		.name = DRV_NAME,
+		.of_match_table = tracectrl_of_match,
+	},
+	.probe = tracectrl_probe,
+	.remove = tracectrl_remove,
+};
+module_platform_driver(tracectrl_driver);
+
+MODULE_AUTHOR("Ola Jeppsson <ola.jeppsson@gmail.com>");
+MODULE_DESCRIPTION("OWL TraceCtrl driver");
+MODULE_VERSION("0.1");
+MODULE_LICENSE("GPL");
